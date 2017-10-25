dcfd67958ec476660800586b0efe25b0f8fb83b9
Clark Du <clark.duxin@gmail.com>
2017-10-25 14:53:46 +0800

    feat: add route trace

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const { resolve } = require('path')
 const resolvePath = (...args) => resolve(__dirname, ...args)
 
 module.exports = function trace (options) {
+  if (options.routeTrace) {
+    this.addPlugin({
+      src: resolvePath('templates/route.js'),
+      fileName: 'event-trace-route.js',
+      options
+    })
+  }
   if (options.apiTrace) {
     this.addPlugin({
       src: resolvePath('templates/directive.js'),
---
+++ templates/route.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export default ({ app: { router } }, inject) => {
+  // eslint-disable-next-line no-constant-condition
+  if (!'<%= options.routeTrace %>') {
+    console.warn('Warn: routeTrace is not found in event-trace module options')
+    return
+  }
+  router.afterEach(function (to, from) {
+    const info = {
+      from: from.name,
+      fromPath: from.path,
+      to: to.name,
+      toPath: to.path,
+      params: to.params,
+      time: new Date()
+    }
+    axios.post('<%= options.routeTrace %>', info)
+  })
+}
